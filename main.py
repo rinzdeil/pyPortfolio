cca7457aa2d674874e965df381d2498cc12afa25
rinzdeil <dalesumande@gmail.com>
2025-09-02 21:55:02 +0800

    edited header columns, added images for Day 21

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import streamlit as st
 
 st.set_page_config(
     page_title = "Portfolio",
-    layout="wide"
+    layout="wide",
+
 )
 
-left, right = st.columns(2)
+intro_left, intro_right = st.columns(2)
 
-with left:
-    st.title("RINZDEIL")
+with intro_left:
+    st.image("images/profile.jpg")
     st.write(f"Here's my Practice Portfolio using Streamlit")
 
-with right:
-    st.text("Here's my Portfolio using Streamlit")
+with intro_right:
+    st.title("RINZDEIL")
+    st.info("""Here's my Portfolio using Streamlit. I am a newbie programmer learning python. 
+    I am using my old Acer laptop to do this. The year is 2025. In the bleak midwinter. Flood control
+    anomalies. Drinking Urbanica coffee with Majii    
+    """)
+
+st.text("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+
+body_left, body_right = st.columns(2)
+
+with body_left:
+    st.title("App Title")
+    st.text("App Description")
+    st.image("images/logo.jpg", width=300)
