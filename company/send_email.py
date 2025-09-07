6eedafbd57f284abc7045e11c0fa11e99a25cfab
rinzdeil <140869339+rinzdeil@users.noreply.github.com>
2025-09-07 19:58:17 +0000

    Add a subject line to email

--- company/send_email.py
+++ company/send_email.py
@@ -10,7 +10,10 @@ email = "dalesumande@gmail.com"
 receiver = "dalesumande@gmail.com"
 context = ssl.create_default_context()
 
-message = "Hi from replit!"
+message = """\
+Subject: Hi from replit!
+This is a test email from python.
+"""
 
 with smtplib.SMTP_SSL(host, port, context = context) as server:
   server.login(email, password)
