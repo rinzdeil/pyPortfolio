b37373c82aa0ebaa3ac68aef000416b5ebf6c018
rinzdeil <140869339+rinzdeil@users.noreply.github.com>
2025-09-08 14:46:44 +0000

    Added topic selection from topics.csv

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,19 +1,25 @@
 import streamlit as st
 import send_email as send
+import pandas as pd
+
+TOPICS_PATH = "./company/topics.csv"
+df = pd.read_csv(TOPICS_PATH)
 
 with st.form(key="email_form"):
-  user_email = st.text_input("Enter your email")
+  user_email = st.text_input("Enter your email", key="email")
+  topic = st.selectbox("What topic do you want to discuss?", df["topic"])
   message_string = st.text_area("Enter your message")
+  submit = st.form_submit_button("Submit")
 
   message = f"""\
 Subject: PyPortfolio Leads
 
 From: {user_email}
+Topic: {topic}
 
 {message_string}
 """
 
-  submit = st.form_submit_button("Submit")
   if submit:
     send.send_email(message)
     st.info("Your email was sent successfully")
